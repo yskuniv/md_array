3f8f45ba0f77226319c1792e71b60c0b5b8b35a8
ysk_univ <ysk.univ.1007@gmail.com>
2020-06-03 21:58:10 +0900

    add spec/md_array/md_array_spec.rb

---
+++ spec/md_array/md_array_spec.rb
@@ -0,0 +1,129 @@
+RSpec.describe MdArray do
+  describe MdArray do
+    describe "size" do
+      where(:size) do
+        [
+          [[]],
+          [[1]],
+          [[2]],
+          [[3]],
+          [[1, 1]],
+          [[1, 2]],
+          [[1, 3]],
+          [[2, 1]],
+          [[2, 2]],
+          [[2, 3]],
+          [[3, 1]],
+          [[3, 2]],
+          [[3, 3]]
+        ]
+      end
+
+      with_them do
+        it "responds correctly" do
+          md = MdArray::MdArray.new(size)
+          expect(md.size).to eq size
+        end
+      end
+    end
+
+    describe "dimension" do
+      where(:size, :answer) do
+        [
+          [[], 0],
+          [[1], 1],
+          [[1, 1], 2]
+        ]
+      end
+
+      with_them do
+        it "responds correctly" do
+          md = MdArray::MdArray.new(size)
+          expect(md.dimension).to eq answer
+        end
+      end
+    end
+
+    describe "[]" do
+      where(:index, :answer) do
+        [
+          [[0, 0, 0], [0, 0, 0]],
+          [[1, 1, 1], [1, 1, 1]],
+          [[2, 2, 2], [2, 2, 2]]
+        ]
+      end
+
+      with_them do
+        it "responds correctly" do
+          md = MdArray::MdArray.new([3, 3, 3]) { |z, y, x| [z, y, x] }
+          expect(md[index]).to eq answer
+        end
+      end
+    end
+
+    describe "adjacent" do
+      where(:index, :strict, :answer) do
+        [
+          [[0, 0, 0], false,
+           [[0, 0, 1], [0, 0, 3],
+            [0, 1, 0], [0, 3, 0],
+            [1, 0, 0], [3, 0, 0]]],
+          [[0, 0, 0], true,
+           [[0, 0, 1],
+            [0, 1, 0],
+            [1, 0, 0]]],
+          [[1, 1, 1], false,
+           [[1, 1, 0], [1, 1, 2],
+            [1, 0, 1], [1, 2, 1],
+            [2, 1, 1], [0, 1, 1]]]
+        ]
+      end
+
+      with_them do
+        it "responds correctly" do
+          md = MdArray::MdArray.new([4, 4, 4]) { |z, y, x| [z, y, x] }
+          expect(md.adjacent(index, strict: strict)).to match_array answer
+        end
+      end
+    end
+
+    describe "neighborhood" do
+      where(:index, :strict, :answer) do
+        [
+          [[0, 0, 0], false,
+           [[3, 3, 3], [3, 3, 0], [3, 3, 1],
+            [3, 0, 3], [3, 0, 0], [3, 0, 1],
+            [3, 1, 3], [3, 1, 0], [3, 1, 1],
+            [0, 3, 3], [0, 3, 0], [0, 3, 1],
+            [0, 0, 3], [0, 0, 1],
+            [0, 1, 3], [0, 1, 0], [0, 1, 1],
+            [1, 3, 3], [1, 3, 0], [1, 3, 1],
+            [1, 0, 3], [1, 0, 0], [1, 0, 1],
+            [1, 1, 3], [1, 1, 0], [1, 1, 1]]],
+          [[0, 0, 0], true,
+           [[0, 0, 1],
+            [0, 1, 0], [0, 1, 1],
+            [1, 0, 0], [1, 0, 1],
+            [1, 1, 0], [1, 1, 1]]],
+          [[1, 1, 1], false,
+           [[0, 0, 0], [0, 0, 1], [0, 0, 2],
+            [0, 1, 0], [0, 1, 1], [0, 1, 2],
+            [0, 2, 0], [0, 2, 1], [0, 2, 2],
+            [1, 0, 0], [1, 0, 1], [1, 0, 2],
+            [1, 1, 0], [1, 1, 2],
+            [1, 2, 0], [1, 2, 1], [1, 2, 2],
+            [2, 0, 0], [2, 0, 1], [2, 0, 2],
+            [2, 1, 0], [2, 1, 1], [2, 1, 2],
+            [2, 2, 0], [2, 2, 1], [2, 2, 2]]]
+        ]
+      end
+
+      with_them do
+        it "responds correctly" do
+          md = MdArray::MdArray.new([4, 4, 4]) { |z, y, x| [z, y, x] }
+          expect(md.neighborhood(index, strict: strict)).to match_array answer
+        end
+      end
+    end
+  end
+end
