144f2e0e88cb08b0a2581d5cafcf0d7a71bbbae5
ysk_univ <ysk.univ.1007@gmail.com>
2022-08-06 21:12:23 +0900

    move lib/utils

---
+++ lib/md_array/utils.rb
@@ -0,0 +1,9 @@
+module MdArray
+  module Utils
+    class << self
+      def partial(f, *args)
+        proc { |*rest_args| f[*args, *rest_args] }
+      end
+    end
+  end
+end
--- lib/utils.rb
+++
@@ -1,7 +0,0 @@
-module Utils
-  class << self
-    def partial(f, *args)
-      proc { |*rest_args| f[*args, *rest_args] }
-    end
-  end
-end
